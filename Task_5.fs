867cdca339002acfe25b7c4c3f6f94a6577fe3e3
Fisher48 <rybakov48@yandex.ru>
2026-03-05 15:23:16 +0300

    Added Task_5

---
+++ Task_5.fs
@@ -0,0 +1,15 @@
+// 16.1
+let notDivisible (n,m) = m % n = 0
+
+// 16.2
+let prime n =
+    let rec maxDivider d =
+        match d with
+        | 1 -> true
+        | _ when n % d = 0 -> false
+        | _ -> maxDivider(d - 1)
+   match n with
+   | n when n < 2 -> false
+   | _ -> maxDivider(n - 1)
+
+
