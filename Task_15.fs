1cbad858d1eaea27b89f0662fd1e5a2e5030dee1
Fisher48 <rybakov48@yandex.ru>
2026-03-18 10:55:01 +0300

    Added Task_15

---
+++ Task_15.fs
@@ -0,0 +1,10 @@
+// 41.4.1
+let list_filter f xs = List.foldBack (fun x acc -> if f x then x :: acc else acc) xs []
+
+// 41.4.2
+let sums (p, xs) = List.fold (fun acc x -> if p x then acc + x else acc) 0 xs
+
+// 41.4.3
+let revrevs lsts = List.fold (fun head tail -> List.rev tail :: head) [] lsts
+
+
