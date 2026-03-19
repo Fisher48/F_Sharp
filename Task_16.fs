6b0882606e8c96fddecfca9963ed6a645934a817
Fisher48 <rybakov48@yandex.ru>
2026-03-19 12:26:28 +0300

    Added Task_16

---
+++ Task_16.fs
@@ -0,0 +1,12 @@
+// 42.3
+let rec allSubsets n k =
+    match n, k with
+    | _,0 -> set [Set.empty]
+    | 0,_ -> set []
+    | n ,k when n = k -> set [ set[1..n] ]
+    | _ -> Set.union
+                (allSubsets (n-1) k)
+                (Set.map (Set.add n) (allSubsets (n-1) (k-1)))
+
+
+    
