1b16dfd93e80b58fab58bd0b4d31f633c9dfe45a
Fisher48 <rybakov48@yandex.ru>
2026-03-24 13:19:21 +0300

    Added Task_21

--- Task_21.fs
+++ Task_21.fs
@@ -3,7 +3,7 @@ let rec fact = function
     | n -> n * fact (n-1)
 
 // 50.2.1
-let fac_seq1 = seq {
+let fac_seq = seq {
     let mutable i = 0
     while true do
         yield fact i
@@ -11,7 +11,7 @@ let fac_seq1 = seq {
 }
 
 // 50.2.2
-let seq_seq2 = seq {
+let seq_seq = seq {
     for i in Seq.initInfinite id do
     if i % 2 = 0 then yield i / 2
     else yield -(i + 1) / 2
